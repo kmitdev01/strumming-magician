c4fa4f49753b5da300e335e420a454bb62513694
dinkarj <dev01.kmit@gmail.com>
2024-05-14 17:58:54 +0530

    added updates for progress bar

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+
+dotenv.config({ path: './config.env' });
+
+app.use(cors({
+  origin: [
+    'http://127.0.0.1:5500',
+  'https://guitar-webapp.netlify.app'
+]
+}));
+
+app.use(express.json())
+app.use(cookieParser())
+app.set("view engine", "ejs");
+app.use(express.static('Assets'));
+
+const DB = process.env.DATABASE;
+mongoose.set("strictQuery", false);
+mongoose.connect(DB).then(() => {
+    console.log('Connected');
+}).catch((e) => { console.log('Not connected' + e); })
+
+app.use(require('./Router/AppFunctioanlity'));
+app.use(require('./Router/Users'));
+app.use(require('./Router/SongDataAPI'));
+app.use(require('./Router/DashboardAPIs'));
+// app.use(require('./Router/HtmlToImg'));
+
+app.listen(PORT, (error) => {
+  if (!error) {
+    console.log("Server is Successfully Running, and App is listening on port " + PORT)
+  } else {
+    console.log('Server not started ' + error);
+  }
+});
+
