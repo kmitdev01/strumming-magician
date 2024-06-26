db62e4bc3d7d3fb783a21b45c622f33ce65e757d
dinkarj <dev01.kmit@gmail.com>
2024-06-26 17:02:56 +0530

    added UI updates

--- Router/UserProfileAPI.js
+++ Router/UserProfileAPI.js
@@ -1,53 +1,63 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
-const UserAccount = require('../Model/UserAccount');
-const SongData = require('../Model/SongData');
+const UserAccount = require("../Model/UserAccount");
+const SongData = require("../Model/SongData");
 
-const {verifyToken, verifiedUserEmail} = require("../Authentiation/verification")
+const {
+  verifyToken,
+  verifiedUserEmail,
+} = require("../Authentiation/verification");
 
-router.post("/change-user-type", verifyToken, verifiedUserEmail, async (req, res) => {
-
+router.post(
+  "/change-user-type",
+  verifyToken,
+  verifiedUserEmail,
+  async (req, res) => {
     try {
+      let userType = req.body.userType;
+      const UID = req.cookies.UID;
 
-        let userType = req.body.userType;
-        const UID = req.cookies.UID;
+      let User = await UserAccount.findOne({ _id: UID });
 
-        let User = await UserAccount.findOne({ _id: UID })
+      User.userType = userType;
 
-        User.userType = userType
+      await User.save();
 
-        await User.save()
-
-        res.send("Updated")
-
+      res.send("Updated");
     } catch (error) {
-        console.log(error);
-        res.send("Error")
+      console.log(error);
+      res.send("Error");
     }
-
-
-})
-router.post("/change-user-password", verifyToken, verifiedUserEmail, async (req, res) => {
-
+  }
+);
+router.post(
+  "/change-user-data",
+  verifyToken,
+  verifiedUserEmail,
+  async (req, res) => {
     try {
+        console.log("running API ---> ", req.body);
+      const newUserName = req.body.username;
+      const newPassword = req.body.password;
+      const UID = req.cookies.UID;
+      let User = await UserAccount.findOne({ _id: UID });
+      User.name = newUserName;
+      if (newPassword) {
+        console.log("running hash ---> ", newPassword);
+        const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+        User.password = hashedNewPassword;
+      }
+      // password update logic here
+      await User.save();
 
-        // let userType = req.body.userType;
-        // const UID = req.cookies.UID;
-        // let User = await UserAccount.findOne({ _id: UID })
-        // User.userType = userType
-        // await User.save()
-
-        // password update logic here
-
-        res.send("Updated")
-
+      res.send("Updated");
     } catch (error) {
-        console.log(error);
-        res.send("Error")
+      console.log(error);
+      res.send("Error");
     }
-
-
-})
+  }
+);
 
 module.exports = router;
